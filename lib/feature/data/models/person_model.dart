7e2f7471c0aab366e13db3c9b8a9af50684dd31d
aitemir <ajtemirulanbekuulu@gmail.com>
2022-04-17 20:56:20 +0600

    view all personages

---
+++ lib/feature/data/models/person_model.dart
@@ -0,0 +1,53 @@
+import 'package:rick_and_morty/feature/data/models/location_model.dart';
+import 'package:rick_and_morty/feature/domain/entities/person_entity.dart';
+
+class PersonModel extends PersonEntity {
+  PersonModel(
+      {required int id,
+      required String name,
+      required String status,
+      required String species,
+      required String type,
+      required String gender,
+      required origin,
+      required location,
+      required String image,
+      required List<String> episode,
+      required DateTime created})
+      : super(
+            id: id,
+            name: name,
+            status: status,
+            species: species,
+            type: type,
+            gender: gender,
+            origin: origin,
+            location: location,
+            image: image,
+            episode: episode,
+            created: created);
+
+  factory PersonModel.fromJson(Map<String, dynamic> json) {
+    return PersonModel(
+        id: json['id'],
+        name: json['name'],
+        status: json['status'],
+        species: json['species'],
+        type: json['type'],
+        gender: json['gender'],
+        origin: json['origin'] != null
+            ? LocationModel.fromJson(json['origin'])
+            : null,
+        location: json['location'] != null
+            ? LocationModel.fromJson(json['location'])
+            : null,
+        image: json['image'],
+        episode:
+            (json['episode'] as List<dynamic>).map((e) => e as String).toList(),
+        created: DateTime.parse(json['created'] as String));
+  }
+
+  Map<String, dynamic> toJson() {
+    return {};
+  }
+}
